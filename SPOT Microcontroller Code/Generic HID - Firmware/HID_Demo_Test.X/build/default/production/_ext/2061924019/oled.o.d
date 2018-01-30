dfb0dc43bccd33006cd0eec41718e63f34beead3
jwatson27 <jwatson27@cedarville.edu>
2018-01-30 15:05:01 -0500

    Added microcontroller firmware

---
+++ SPOT Microcontroller Code/Generic HID - Firmware/HID_Demo_Test.X/build/default/production/_ext/2061924019/oled.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/2061924019/oled.o:  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/OLED\ driver/oled.c  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/OLED\ driver/oled.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/GenericTypeDefs.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/Compiler.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Generic\ HID\ -\ Firmware/p18f46j50.h  \
+
---
+++ SPOT Microcontroller Code/Generic HID - Firmware/HID_Demo_Test.X/build/default/production/_ext/713069093/usb_device.o.d
@@ -0,0 +1,19 @@
+build/default/production/_ext/713069093/usb_device.o:  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Usb/usb_device.c  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_common.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_device.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/GenericTypeDefs.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/Compiler.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Generic\ HID\ -\ Firmware/p18f46j50.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_ch9.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Generic\ HID\ -\ Firmware/usb_config.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_ch9.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_hal.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_common.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_hal_pic18.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_hal.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Microchip/Include/USB/usb_device.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Generic\ HID\ -\ Firmware/HardwareProfile.h  \
+	C:/Users/jwatson27/Desktop/HID\ Demo\ Lab/Generic\ HID\ -\ Firmware/HardwareProfile\ -\ PIC18F\ Starter\ Kit.h  \
+
